7055d4bfc969657f1202c621c276f99183a0fe63
piotr-kalanski <piotr.kalanski@gmail.com>
2017-12-01 23:37:03 +0100

    Write JSON message

---
+++ producer-java-api/src/main/java/com/datawizards/kafka/producer/WriteSyncJsonMessage.java
@@ -0,0 +1,38 @@
+package com.datawizards.kafka.producer;
+
+import com.datawizards.kafka.model.Person;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class WriteSyncJsonMessage {
+
+    private static final String TOPIC = "test_json";
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put("client.id", "write-json-message");
+        config.put("bootstrap.servers", "localhost:9092");
+        config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        config.put("value.serializer", "org.apache.kafka.connect.json.JsonSerializer");
+        config.put("acks", "all");
+
+        KafkaProducer<String, JsonNode> producer = new KafkaProducer<String, JsonNode>(config);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        for(int i=0;i<100;++i) {
+            try {
+                JsonNode  jsonNode = objectMapper.valueToTree(new Person(i, "person_" + i));
+                ProducerRecord<String, JsonNode> record = new ProducerRecord<String, JsonNode>(TOPIC, i + "", jsonNode);
+                producer.send(record).get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
