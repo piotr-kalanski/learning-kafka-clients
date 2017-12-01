72ce33ae35c698a562b3802a933cca30c4fb72ea
piotr-kalanski <piotr.kalanski@gmail.com>
2017-12-01 22:46:12 +0100

    initial repo structure

--- producer-java-api/src/main/java/com/datawizards/kafka/WriteStringMessage.java
+++ producer-java-api/src/main/java/com/datawizards/kafka/WriteStringMessage.java
@@ -2,6 +2,6 @@ package com.datawizards.kafka;
 
 public class WriteStringMessage {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("hello");
     }
 }
